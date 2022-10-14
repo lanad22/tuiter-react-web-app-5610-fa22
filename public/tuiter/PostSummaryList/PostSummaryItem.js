b420634fc26b266b25e08f084f0173a9c91f3727
lanado <pldo2@illinois.edu>
2022-10-14 13:23:35 -0700

    a5 JavaScript sp22

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -2,16 +2,16 @@ const PostSummaryItem = (post) => {
     return (`
        <li class="list-group-item">
                     <div class="row pe-3">
-                        <div class="col-10">   
-                            <span class="text-secondary">
-                                ${post.topic}</span>         
-                            <p class="fw-bold text-white">
-                                ${post.userName} <i class="fa fa-check-circle"></i> - 2h<br/>
-                                ${post.title}
-                            </p>
+                        <div class="col-10 text-secondary">   
+                             ${post.topic} 
+                             <p>  
+                                <span class= "fw-bold text-white">${post.userName}
+                                <i class="fa fa-check-circle"></i> </span> - ${post.time}<br/>
+                                <span class= "fw-bold text-white">${post.title}</span>    
+                             </p>
                         </div>
                         <div class="col-2 p-0">
-                            <img src="${post.image}" class="img-fluid">
+                            <img src="${post.image}" class="img-fluid rounded">
                         </div>
                     </div>
                 </li>
